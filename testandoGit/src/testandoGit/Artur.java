9cc4c2f55e6ad8a78b9629dee00dce2034b82a85
artur <artur@artur>
2020-02-13 12:09:31 -0200

    Programa implementado

---
+++ testandoGit/src/testandoGit/Artur.java
@@ -0,0 +1,17 @@
+package testandoGit;
+
+public class Artur {
+	
+	public static int a = 2;
+	public static int b = 3;
+	public int c;
+	
+	public static int soma(int a, int b){
+		  int c = a + b;
+		  return c;
+		}
+
+	public static void main(String[] args) {
+		System.out.println(soma(a,b));
+	}
+}
